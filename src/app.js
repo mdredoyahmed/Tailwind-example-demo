02304331a510c7511dd2c9b50037e793195ff112
Md Hridoy Ahmed <95656155+mdredoyahmed@users.noreply.github.com>
2025-06-27 01:04:40 +0600

    buttonm add

--- src/app.js
+++ src/app.js
@@ -9,21 +9,21 @@ const filterAllBtn = document.getElementById("fillter-all") ;
 const filterActiveBtn= document.getElementById("fillter-active ") ;
 const filterCompletedBtn = document.getElementById("fillter-completed ") ;
 
-const currentFilter = " all" ;
+let currentFilter = "all" ;
 
-async function loadTodos(params) {
+async function loadTodos() {
     const url = API ;
-    if(currentFilter ==="activ" ){
+    if(currentFilter ==="active" ){
         url +="?isCompleted=false";
     }else if (currentFilter === "completed"){
         url +="?isCompleted=true" ;
     }
 
-    try{
+    try {
        const res = await fetch(url);
        const todos = await res.json();
-       console.log(todos);\
-       renderTodos(todos)
+       console.log(todos);
+       renderTodos(todos);
 
     }
     catch(error) {
@@ -44,6 +44,38 @@ async function loadTodos(params) {
        li.className = "flex items-center gap-3" ;
        const checkBox = document.createElement('input');
        checkBox.type = 'checkbox' ;
+       checkBox.checked = todo.isCompleted;
+       checkBox.className = " w-5 h-5 cursor-pointer" ;
+    //    todo add even lisenar in the check box toggling the completed Flag 
+    
+     checkBox.addEventListener("change" , ()=> {
+        toggleIsCompleted(todo.id,checkBox.checked);
+     })
+
+    const leftDiv = document.createElement("div");
+    leftDiv.classList = "flex items-center gap-3" ;
+    
+    const title = document.createElement("span");
+    title.textContent = todo.title;
+
+    title.className = todo.isCompleted?"line-through text-gray-400":"" ;
+
+    leftDiv.appendChild(checkBox) ;
+    leftDiv.appendChild(title)
+        
+    const delBtn = document.createElement("button") ;
+    delBtn.textContent = "Delete Todo" ;
+    delBtn.className = " bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600" ;
+    li.appendChild(leftDiv) ;
+    li.appendChild(delBtn) ;
+
+    todoaList.appendChild(li);
+
     });
  }
+
+async function toggleIsCompleted(id , completed) {
+    await fetch(`${API}`)
+}
+
  loadTodos();
